2fb06adeb79468e1e6478e3385b0ca88f2a8e241
Chandan <cs@tectusdreamlab.com>
2019-03-08 15:40:43 +0800

    Added test cases

---
+++ SamplePodCreationTest/AdditionHandler.swift
@@ -0,0 +1,23 @@
+//
+//  AdditionHandler.swift
+//  SamplePodCreationTest
+//
+//  Created by Chandan on 8/3/19.
+//  Copyright © 2019 Chandan. All rights reserved.
+//
+
+import Foundation
+
+public final class AdditionHandler {
+    
+    let name = "AdditionHandler"
+    
+    public func add(a: Int, b: Int) -> Int {
+        return a + b
+    }
+    
+    public func sub(a: Int, b: Int) -> Int {
+        return a - b
+    }
+    
+}
--- SamplePodCreationTestTests/SamplePodCreationTestTests.swift
+++ SamplePodCreationTestTests/SamplePodCreationTestTests.swift
@@ -11,24 +11,17 @@ import XCTest
 
 class SamplePodCreationTestTests: XCTestCase {
 
+    var additionHandler: AdditionHandler!
+    
     override func setUp() {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+        additionHandler = AdditionHandler()
+    }
+    
+    func testAdd() {
+        XCTAssertEqual(additionHandler.add(a: 1, b: 1), 2)
     }
 
-    override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    func testSub() {
+        XCTAssertEqual(additionHandler.sub(a: 2, b: 1), 1)
     }
-
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
-
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
-    }
-
 }
